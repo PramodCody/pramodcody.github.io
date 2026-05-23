7908424e72ca9d0c9f1c34fb34f18e1f141594a3
PramodCody <pramodprajapatipk2@gmail.com>
2026-05-23 17:09:48 +0545

    Corrected assests spelling to assets, also maybe download faster maybe

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const download_button = document.getElementById("download_apk_button");
 download_button.addEventListener('click', function(){
 
     const download_link = document.createElement('a'); //a storage space created
-    download_link.href = "assests/Kids_Classium.apk"; //href = file location
+    download_link.href = "assets/Kids_Classium.apk"; //href = file location
     download_link.download = "Kids_Classium.apk"; //download = file name
     download_link.click();
 
