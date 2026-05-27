55f860bdde2f606b811e32b647d403ff1c7d2f31
PramodCody <pramodprajapatipk2@gmail.com>
2026-05-27 15:42:12 +0545

    again PramodCody 1.0

--- download.js
+++ download.js
@@ -9,21 +9,21 @@ function triggerFadeOut() {
 download_button.addEventListener('click', triggerFadeOut);
 
 
-// download_button.onclick = () => {
+download_button.onclick = () => {
 
-//     // Create a temporary anchor element
-//     const link = document.createElement('a');
+    // Create a temporary anchor element
+    const link = document.createElement('a');
     
-//     // Set the path to the asset
-//     link.href = 'assets/Kids_Classium.apk';
+    // Set the path to the asset
+    link.href = 'assets/Kids_Classium.apk';
     
-//     // Specify the filename for the download
-//     link.download = 'Kids Classium.apk';
+    // Specify the filename for the download
+    link.download = 'Kids Classium.apk';
     
-//     // Append to body, trigger click, and remove from DOM
-//     document.body.appendChild(link);
-//     link.click();
-//     document.body.removeChild(link);
-//     // download_signal.classList.remove('fade-out-active');
-// };
+    // Append to body, trigger click, and remove from DOM
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    // download_signal.classList.remove('fade-out-active');
+};
 
